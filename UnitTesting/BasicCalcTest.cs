d3f08f8dc111a8b370eb3a60270163831b318ba0
serveladkas <serveladkas@gmail.com>
2024-11-10 22:29:48 +0300

    labwork7

--- UnitTesting/BasicCalcTest.cs
+++ UnitTesting/BasicCalcTest.cs
@@ -40,45 +40,7 @@ namespace UnitTesting
             Assert.Throws<DivideByZeroException>(() => _calculator.Divide(2, 0));
         }
 
-        [Fact]
-        public void Factorial_ShouldReturnCorrectAnswer()
-        {
-            var result = _calculator.Factorial(3);
-            Assert.Equal(6, result);
-        }
-
-        [Theory]
-        [InlineData(3, 6)]
-        [InlineData(0, 1)]
-        [InlineData(5, 120)]
-        public void Factorial_Theory(int a, int expectedResult)
-        {
-            var result = _calculator.Factorial(a);
-            Assert.Equal(expectedResult, result);
-        }
-
-        [Fact]
-        public void Factorial_ShouldThrowArgumentOutOfRangeException()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.Factorial(-2));
-        }
-
-        [Theory]
-        [InlineData(1, -3, 2, 2.0, 1.0)]
-        [InlineData(1.0, -2.0, 1.0, 1.0, 1.0)]
-        [InlineData(1.0, 0, 1.0, null, null)]
-        public void SolveQuadraticEquation_ShouldReturnCorrectRoots(double a, double b, double c, double? expectedFirstRoot, double? expectedSecondRoot)
-        {
-            var (firstRoot, secondRoot) = _calculator.SolveQuadraticEquation(a, b, c);
-            Assert.Equal(expectedFirstRoot, firstRoot);
-            Assert.Equal(expectedSecondRoot, secondRoot);
-        }
-
-        [Fact]
-        public void SolveQuadraticEquation_ArgumentOutOfRangeException()
-        {
-            Assert.Throws<ArgumentOutOfRangeException>(() => _calculator.SolveQuadraticEquation(0, 2, 4));
-        }
+        
     }
 }
 
---
+++ UnitTesting/serveladkas/ShopServiceTest.cs
@@ -0,0 +1,88 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestingLib.Shop;
+
+namespace UnitTesting.serveladkas
+{
+    public class ShopServiceTest
+    {
+        private readonly Mock<ICustomerRepository> _mockCustomerRepository;
+        private readonly Mock<IOrderRepository> _mockOrderRepository;
+        private readonly Mock<INotificationService> _mockNotificationService;
+        private readonly ShopService _shopService;
+
+        public ShopServiceTest()
+        {
+            _mockCustomerRepository = new Mock<ICustomerRepository>();
+            _mockOrderRepository = new Mock<IOrderRepository>();
+            _mockNotificationService = new Mock<INotificationService>();
+            _shopService = new ShopService(_mockCustomerRepository.Object, _mockOrderRepository.Object, _mockNotificationService.Object);
+        }
+
+        [Fact]
+        public void CreateOrder_ShouldAddOrder()
+        {
+            var customer = new Customer { Id = 1, Name = "ааа", Email = "ааааа" };
+            var order = new Order { Id = 1, Amount = 100, Customer = customer };
+
+            _shopService.CreateOrder(order);
+
+            _mockOrderRepository.Verify(repo => repo.AddOrder(order), Times.Once);
+        }
+
+        [Fact]
+        public void SendNotification_ShouldSendCorrectNotification()
+        {
+            var customer = new Customer { Id = 1, Name = "ааа", Email = "ааааа" };
+            var order = new Order { Id = 1, Amount = 100, Customer = customer };
+
+            _shopService.CreateOrder(order);
+
+            _mockNotificationService.Verify(service => service.SendNotification(
+                order.Customer.Email,
+                $"Order {order.Id} created for customer {order.Customer.Name} total price {order.Amount}"), Times.Once);
+        }
+
+        [Fact]
+        public void GetCustomerInfo_ShouldReturnCorrectInfo()
+        {
+            var customer = new Customer { Id = 1, Name = "ааа" };
+            var orders = new List<Order> { new Order { Customer = customer }, new Order { Customer = customer } };
+
+            _mockCustomerRepository.Setup(repo => repo.GetCustomerById(1)).Returns(customer);
+            _mockOrderRepository.Setup(repo => repo.GetOrders()).Returns(orders);
+
+            var result = _shopService.GetCustomerInfo(1);
+
+            Assert.Equal("Customer ааа has 2 orders", result);
+        }
+
+        [Fact]
+        public void CreateOrder_ArgumentException()
+        {
+            var existingOrder = new Order { Id = 1, Customer = new Customer { Email = "test@example.com", Name = "John Doe" }, Amount = 100 };
+            _mockOrderRepository.Setup(repo => repo.GetOrderById(existingOrder.Id)).Returns(existingOrder);
+
+            var exception = Assert.Throws<ArgumentException>(() => _shopService.CreateOrder(existingOrder));
+            Assert.Equal("Order with current id already exists", exception.Message);
+        }
+
+        [Fact]
+        public void GetCustomerInfo_ShouldReturnCorrectInfoWhenCustomerHasNoOrders()
+        {
+            var customer = new Customer { Id = 1, Name = "AAAAAAAAAAAAAA" };
+            var orders = new List<Order>();
+
+            _mockCustomerRepository.Setup(repo => repo.GetCustomerById(1)).Returns(customer);
+            _mockOrderRepository.Setup(repo => repo.GetOrders()).Returns(new List<Order>());
+            var sykablyad = "заказик";
+            var result = _shopService.GetCustomerInfo(1);
+
+            Assert.Equal("Customer AAAAAAAAAAAAAA has 0 orders", result);
+        }
+    }
+}
